0326d7ee9245d905b978b5c70586f973f0fc0bae
Bhargav Maniar <bmania@sapient.com>
2017-03-09 10:38:17 +0530

    footer bug solved

--- app/shared/index_footer.js
+++ app/shared/index_footer.js
@@ -2,14 +2,14 @@
         template: `
         <div class="container-fluid row "> 
         <div class= "col-xs-12 footerClass">
-        <div class="pull-left leftFooter">
+        <span class="pull-left leftFooter">
              <button type="button" class="btn btn-link">Copyright</button>
              <button type="button" class="btn btn-link">About</button>
-        </div>
-        <div class="pull-right rightFooter col-md-offset-2 col-xs-4">
+        </span>
+        <span class="pull-right rightFooter">
              <button type="button" class="btn btn-link">Terms</button>
              <button type="button" class="btn btn-link">Privacy</button>
              <button type="button" class="btn btn-link">Contact Us</button>
-        </div>
+        </span>
         </div>`
     })
